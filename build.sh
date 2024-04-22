bf246789bc4161b1c85179a0dec6e308945e09a1
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-04-22 08:43:17 -0700

    Updated mapping format

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+echo "const hitop_mappings = $(cat data/hitop-mappings.json | jq --compact-output .);"
 echo "const hitop_sr = $(cat data/hitop-sr.json | jq --compact-output .);"
-echo "const whodas = $(cat data/whodas-full.json | jq --compact-output .);"
+echo "const whodas_full = $(cat data/whodas-full.json | jq --compact-output .);"
+echo "const whodas_short = $(cat data/whodas-short.json | jq --compact-output .);"
